3a2c7f5b5b21c01d12ec3f99fecf79abc4884931
nharding925 <nharding925@gmail.com>
2026-02-12 14:03:38 -0500

    Add new message

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,7 @@
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Im practicing git");
+        System.out.println("Im practicing git again");
 
     }
 }
