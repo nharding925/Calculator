cb482a7295c1ad17c0ab79ba54aabcc704ae95cc
nharding925 <nharding925@gmail.com>
2026-02-12 14:17:49 -0500

    added support for floating point numbers, fixed missing print statement for subtraction

--- Calculator.java
+++ Calculator.java
@@ -5,29 +5,29 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         
         System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
+        double num1 = scanner.nextDouble();
         
         System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
+        double num2 = scanner.nextDouble();
         
         System.out.print("Choose an operation (addition(+), subtraction(-), multiplication(*) or division(/): ");
         String operation = scanner.next();
         
         if (operation.equals("+")) {
-            int result = num1 + num2;
+            double result = num1 + num2;
             System.out.println("The sum is: " + result);
         } else if (operation.equals("-")) {
-            int result = num1 - num2;
+            double result = num1 - num2;
+            System.out.println("The difference is: " + result);
         } else if (operation.equals("*")) {
-            int result = num1 * num2;
+            double result = num1 * num2;
             System.out.println("The product is: " + result);
         } else if (operation.equals("/")) {
-            int result = num1 / num2;
+            double result = num1 / num2;
             System.out.println("The quotient is: " + result);
         } else {
             System.out.println("Invalid operation selected.");
         }
-        
         
         scanner.close();
     }
